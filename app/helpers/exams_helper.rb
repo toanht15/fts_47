14db1aafc22a87578ca6e10b536100253273b2be
Hoang The Toan <toanht15@gmail.com>
2016-01-07 11:08:43 +0700

    resf #50774 fix exam status layout

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -1,5 +1,4 @@
 module ExamsHelper
-
   def check_correct_answer result
     if result.question.text?
       result.content_answer[0] == result.question.answers.correct_answer[0].content
@@ -7,6 +6,19 @@ module ExamsHelper
       (result.question.answers.correct_answer.ids.map{|id| id.to_s} -
         result.content_answer).empty? && result.question.answers.correct_answer.size ==
         result.content_answer.size
+    end
+  end
+
+  def status_exam exam
+    case exam.status
+    when Settings.exam.status.start
+      content_tag :span, t("exam.status.start"), class: "label label-primary"
+    when Settings.exam.status.checked
+      content_tag :span, t("exam.status.checked"), class: "label label-success"
+    when Settings.exam.status.uncheck
+      content_tag :span, t("exam.status.uncheck"), class: "label label-info"
+    when Settings.exam.status.testing
+      content_tag :span, t("exam.status.testing"), class: "label label-warning"
     end
   end
 end
